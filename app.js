50a52c0cecfb090123377c66c16de870359ba406
fabioCTD <fabioaugustoctd@gmail.com>
2021-12-08 17:14:57 -0300

    Fim do trabalho

---
+++ app.js
@@ -0,0 +1,27 @@
+let curso = require('./curso');
+
+function Aluno(nome, quantidadeFaltas, notas){
+    this.nome = nome;
+    this.quantidadeFaltas = quantidadeFaltas;
+    this.notas = notas;
+    this.faltas = function () {
+        this.quantidadeFaltas += 1;
+    },
+    this.calcularMedia = function () {
+        let soma = 0;
+        for (let i = 0; i < this.notas.length; i++) {
+            soma += this.notas[i];
+        }
+        return soma / this.notas.length;
+    }
+}
+
+let aluno1 = new Aluno('Fernando', 10, [7.8, 8, 8]);
+let aluno2 = new Aluno('Vanessa', 5, [6, 5, 7]);
+let aluno3 = new Aluno('Joubert', 8, [8, 6, 7]);
+
+curso.curso.novoEstudante(aluno1);
+curso.curso.novoEstudante(aluno2);
+curso.curso.novoEstudante(aluno3);
+
+console.log(curso.curso.listDaVerdade())
